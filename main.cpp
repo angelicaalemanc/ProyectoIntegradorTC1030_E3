61c4d48bb0dc7627331cae55558fb5ff96aa7539
DamianAlbino <83721825+DamianAlbino@users.noreply.github.com>
2021-06-11 19:16:51 -0600

    Actualización main.cpp
    Se añade una prueba extra al main.

--- main.cpp
+++ main.cpp
@@ -38,10 +38,23 @@ int main(){
   S2T2.enlistarEpisodio({S2_E21,S2_E22});
   Serie S2(4, "Lucifer", 3, "Ficcion", {S2T1,S2T2});
   
-  VT1.agregarVideo({S1});
-  VT1.agregarVideo({S2});
+  Temporada S3T1(1, 0, {});
+  Temporada S3T2(2, 0, {});
+  S3T1.evaluar(90);
+  S3T2.evaluar(92);
+  Episodio S3_E11("A Hard Day's Night");
+  Episodio S3_E12("The First Cut is the Deepest");
+  Episodio S3_E21("Raindrops Keep Falling on My Head"); 
+  Episodio S3_E22("Enough is Enough (No More Tears)");
+  S3T1.enlistarEpisodio({S3_E11,S3_E12});
+  S3T2.enlistarEpisodio({S3_E21,S3_E22});
+  Serie S3(5, "Grey's Anatomy", 3, "Drama", {S3T1,S3T2});
+
   VT1.agregarVideo({P1});
   VT1.agregarVideo({P2});
+  VT1.agregarVideo({S1});
+  VT1.agregarVideo({S2});
+  VT1.agregarVideo({S3});
   VT1.enlistar();
 
   return 0;
